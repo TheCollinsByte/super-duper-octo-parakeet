5f2d12fc5f4529c137c63e994841ff9fcf99ea6b
colspan <collin@collindigm.com>
2023-11-01 17:44:46 +0300

    Sorting Method Implemented

--- sorting/src/main/java/com/collindigm/sorts/Sort.java
+++ sorting/src/main/java/com/collindigm/sorts/Sort.java
@@ -1,7 +1,26 @@
 package com.collindigm.sorts;
 
+import java.util.Arrays;
+
 public class Sort {
+    static String[] BubbleSort(String[] values) {
+        var size = values.length;
+        while (size > 1) {
+            for (var j = 0; j < size - 1; j++) {
+                if (values[j].compareTo(values[ j + 1 ]) > 0) {
+                    final var tmp = values[j + 1];
+                    values[j + 1] = values[j];
+                    values[j] = tmp;
+                }
+            }
+            size--;
+        }
+        return values;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Sorting Module");
+        String[] unsorted = {"John", "Zed", "Abraham", "Matt", "Ninja", "Kelvin", "Jane"};
+        String[] sorted = BubbleSort(unsorted);
+        System.out.println(Arrays.stream(sorted).toList());
     }
 }
